2b9965f4202b81908a922bdd802281723d8d1272
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-01-17 12:02:51 -0600

    finished take a number battle of the bands practice in functions chapter

---
+++ main.js
@@ -0,0 +1,12 @@
+let bandNumber = 1;
+
+const takeNumber = function (band) {
+    console.log(bandNumber + ". " + `${band}`);
+    bandNumber++
+    }
+
+takeNumber("The Maine");
+takeNumber("All Time Low");
+takeNumber("State Champs");
+takeNumber("Mayday Parade");
+takeNumber("Paramore");
